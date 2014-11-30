aa702123294c91236a6e448ad10bc65de5c43c5f
Tushar Kapila <tgkprog@gmail.com>
2014-11-30 11:18:05 +0400

    fix for 3490, format for number

---
+++ src/main/java/meet/Greeter.java
@@ -0,0 +1,7 @@
+package meet;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/meet/SayHi.java
@@ -0,0 +1,8 @@
+package meet;
+
+public class SayHi {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
